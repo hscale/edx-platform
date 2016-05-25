1415ec68fa49020d5e8f68a248225a42f04b5485
Brian Wilson <brian@edx.org>
2016-05-25 16:14:24 -0400

    Add more tests.

--- cms/djangoapps/contentstore/management/commands/tests/test_export_olx.py
+++ cms/djangoapps/contentstore/management/commands/tests/test_export_olx.py
@@ -1,9 +1,15 @@
 """
 Tests for exporting OLX content.
 """
-import unittest
-import shutil
+
 import ddt
+from path import Path as path
+import shutil
+from StringIO import StringIO
+import tarfile
+from tempfile import mkdtemp
+import unittest
+
 from django.core.management import CommandError, call_command
 
 from xmodule.modulestore.tests.factories import CourseFactory
@@ -31,22 +37,8 @@ class TestArgParsingCourseExportOlx(unittest.TestCase):
 @ddt.ddt
 class TestCourseExportOlx(ModuleStoreTestCase):
     """
-    Test exporting a course
+    Test exporting OLX content from a course or library.
     """
-    def setUp(self):
-        super(TestCourseExportOlx, self).setUp()
-
-    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
-    def test_export_course_with_directory_name(self, store):
-        """
-        Create a new course try exporting in a path specified
-        """
-        course = CourseFactory.create(default_store=store)
-        course_id = unicode(course.id)
-        self.assertTrue(
-            modulestore().has_course(course.id),
-            "Could not find course in {}".format(store)
-        )
 
     def test_invalid_course_key(self):
         """
@@ -71,3 +63,43 @@ class TestCourseExportOlx(ModuleStoreTestCase):
         errstring = "Invalid course_id"
         with self.assertRaisesRegexp(CommandError, errstring):
             call_command('export_olx', 'x/y/z', 'dummy_output_path')
+
+    def create_dummy_course(self, store_type):
+        """Create small course."""
+        course = CourseFactory.create(default_store=store_type)
+        self.assertTrue(
+            modulestore().has_course(course.id),
+            "Could not find course in {}".format(store_type)
+        )
+        return course.id
+
+    def check_export_file(self, tar_file, course_key):
+        """Check content of export file."""
+        names = tar_file.getnames()
+        dirname = "{0.org}-{0.course}-{0.run}".format(course_key)
+        self.assertIn(dirname, names)
+        # Check if some of the files are present, without being exhaustive.
+        self.assertIn("{}/about".format(dirname), names)
+        self.assertIn("{}/about/overview.html".format(dirname), names)
+        self.assertIn("{}/assets/assets.xml".format(dirname), names)
+        self.assertIn("{}/policies".format(dirname), names)
+
+    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
+    def test_export_course(self, store_type):
+        test_course_key = self.create_dummy_course(store_type)
+        tmp_dir = path(mkdtemp())
+        self.addCleanup(shutil.rmtree, tmp_dir)
+        filename = tmp_dir / 'test.tar.gz'
+        call_command('export_olx', unicode(test_course_key), filename)
+        with tarfile.open(filename) as tar_file:
+            self.check_export_file(tar_file, test_course_key)
+
+    @ddt.data(ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split)
+    def test_export_course_stdout(self, store_type):
+        test_course_key = self.create_dummy_course(store_type)
+        out = StringIO()
+        call_command('export_olx', unicode(test_course_key), '-', stdout=out)
+        out.seek(0)
+        output = out.read()
+        with tarfile.open(fileobj=StringIO(output)) as tar_file:
+            self.check_export_file(tar_file, test_course_key)
